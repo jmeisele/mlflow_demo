bf806de2be45b6b2af8c89f4f4e7a64851805905
jmeisele <jmeisele@yahoo.com>
2020-08-13 14:23:57 -0500

    :rocket:

--- mlflow_train.py
+++ mlflow_train.py
@@ -45,8 +45,8 @@ if __name__ == "__main__":
     train_y = train[["quality"]]
     test_y = test[["quality"]]
 
-    alpha = float(sys.argv[1]) if len(sys.argv) > 1 else 0.5
-    l1_ratio = float(sys.argv[2]) if len(sys.argv) > 2 else 0.5
+    alpha = float(sys.argv[1]) if len(sys.argv) > 1 else 0.6
+    l1_ratio = float(sys.argv[2]) if len(sys.argv) > 2 else 0.6
     
     # Set the uri to the remote MLFlow server
     mlflow.set_tracking_uri('https://mlflow.ds.us-east-1.shipt.com/')
